9c4b7f77cb0e2f925682edf2f389f91ae5ad280c
aecastillo101 <105985274+aecastillo101@users.noreply.github.com>
2025-02-16 01:45:20 -0600

    Day 2 of shipping something once a day

---
+++ day2/day2.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('button1')
+const text = document.getElementById('b_position')
+
+
+button.addEventListener("click", action)
+
+
+function action(){
+
+    text.innerHTML = "<h1> This is Day 2 and I'm sick right now! D; </h1> <h1> I am playing with Javascript and buttons</h1>"
+
+}
